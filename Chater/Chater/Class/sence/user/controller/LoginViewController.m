215ccf7a0b24cd7214a29f099b5668ea8604f534
郑建文 <zhengjianwen@outlook.com>
2015-10-13 18:10:48 +0800

    [update] 登陆界面

--- Chater/Chater/Class/sence/user/controller/LoginViewController.m
+++ Chater/Chater/Class/sence/user/controller/LoginViewController.m
@@ -16,6 +16,11 @@
 @property (weak, nonatomic) IBOutlet UITextField *txt4UserName;
 
 @property (weak, nonatomic) IBOutlet UITextField *txt4PassWord;
+@property (weak, nonatomic) IBOutlet UIImageView *welcomeImg;
+@property (weak, nonatomic) IBOutlet UIButton *btn4login;
+@property (weak, nonatomic) IBOutlet UIButton *btn4register;
+@property (weak, nonatomic) IBOutlet UIImageView *backImageView;
+
 
 @property (nonatomic,strong)  CLLocationManager * locationManager;
 
@@ -32,10 +37,72 @@
     [self.locationManager requestWhenInUseAuthorization];
     [self.locationManager startUpdatingLocation];
     
-    self.point = self.locationManager.location;
+    if (self.locationManager.location) {
 
-    // Do any additional setup after loading the view.
+        self.point = [AVGeoPoint geoPointWithLocation:self.locationManager.location];
+    }
+
+    
+    [self buildUI];
+
 }
+
+- (void)buildUI{
+    
+    //隐藏状态栏
+    [self.navigationController setNavigationBarHidden:YES];
+    
+    //设置导航栏透明
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"backgroup"] forBarMetrics:UIBarMetricsCompact];
+    [self.navigationController.navigationBar setShadowImage:[UIImage new]];
+    
+    
+    //背景图片
+    [self.backImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+    
+    
+    //欢迎图片
+    [self.welcomeImg mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.backImageView).offset(120);
+        make.centerX.equalTo(self.backImageView);
+        make.height.equalTo(@40);
+    }];
+    
+    //用户名输入框
+    [self.txt4UserName mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.backImageView);
+        make.top.equalTo(self.welcomeImg.mas_bottom).offset(50);
+        make.width.equalTo(self.backImageView).multipliedBy(0.7);
+    }];
+    
+    //密码输入框
+    [self.txt4PassWord mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.backImageView);
+        make.top.equalTo(self.txt4UserName.mas_bottom).offset(20);
+        make.width.equalTo(self.txt4UserName);
+    }];
+    
+    //登陆按钮
+    [self.btn4login mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.backImageView);
+        make.top.equalTo(self.txt4PassWord.mas_bottom).offset(20);
+        make.width.equalTo(self.txt4UserName);
+    }];
+    
+    
+    //注册按钮
+    [self.btn4register mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.backImageView);
+        make.top.equalTo(self.btn4login.mas_bottom).offset(10);
+    }];
+    
+    
+    
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -61,6 +128,12 @@
     }];
     
 }
+
+#pragma mark - 隐藏状态栏
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+}
+
 
 #pragma mark - CLLocationManagerDelegate
 - (void)locationManager:(nonnull CLLocationManager *)manager didUpdateLocations:(nonnull NSArray *)locations{
