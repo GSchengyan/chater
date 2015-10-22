0d46a7c9eae03363103678b03dc0bac596f8828b
童浩 <haohao575@163.com>
2015-10-22 17:14:28 +0800

    no message

--- Chater/Chater/Class/sence/firendShare/FriendShareController.m
+++ Chater/Chater/Class/sence/firendShare/FriendShareController.m
@@ -7,7 +7,7 @@
 //
 
 #import "FriendShareController.h"
-
+#import "PutFriendVC.h"
 @interface FriendShareController ()
 
 @end
@@ -17,16 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+//    self.navigationItem
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)pusPutFriendVC:(UIBarButtonItem *)sender {
+    PutFriendVC *PFVC = [[PutFriendVC alloc]init];
+    [self.navigationController pushViewController:PFVC animated:YES];
 }
 
 #pragma mark - Table view data source
